a80604145bb3ebde3eea4610a8dbb8b46c67eaf1
LuckyToast <LuckyToast@github.com>
2023-10-22 20:56:17 +0200

    not much changed

--- src/index.ts
+++ src/index.ts
@@ -19,49 +19,41 @@ let log = new Logger('./index.ts')
 
 AppDataSource.initialize().then(async () => {
     server.get('/api/thread', async (_, res) => res.send(await getThread()))
-
     server.get('/api/media/:id', async (req, res) => res.send(await getMediaByPostId(parseInt(req.params.id))))
-
     server.post('/api/insert', upload.single('file'), async (req, res) => {
-        await insert(req.body.msg, req.file? req.file: null).catch(err => res.status(500).send())
+        await insert(req.body.msg, req.file? req.file: null).catch(err => res.status(500).send(err.json()))
         res.status(201).send()
     })
 
-    server.listen(PORT, () => console.log(`=> Express running on port ${PORT}`))
+    server.listen(PORT, () => console.log('=> Server running'))
 }).catch(error => log.handle(error))
 
 
 async function getThread(): Promise<any> {
     return await AppDataSource.manager.find(Post)
-        .catch(err => log.handle(err, 'getThread', 'couldnt get posts from DB'))
+        .catch(err => log.handle(err, 'getThread'))
 }
 
 
 async function getMediaByPostId(id: number): Promise<Buffer> {
     return await AppDataSource.manager.getRepository(Media)
         .query(`select data where post_id = ${id}`)
-        .catch(err => log.handle(err, 'getMediaByPostId', 'couldnt get media file from DB'))
+        .catch(err => log.handle(err, 'getMediaByPostId'))
 }
 
-async function insert(msg: string, file?: Express.Multer.File) {
+
+async function insert(msg: string, f?: Express.Multer.File) {
     const p = new Post()
     p.msg = msg
-    if (file) {
-        if (file.size > 2097152) throw new Error(`${file.originalname} exceeds 2MB max size`)
-        if (!MIMES.includes(file.mimetype)) throw new Error(`${file.originalname} of type ${file.mimetype} not supported`)
+    if (f) {
+        if (f.size > 2097152) throw new Error(`${f.originalname} exceeds 2MB max size`)
+        if (!MIMES.includes(f.mimetype)) throw new Error(`${f.originalname} of type ${f.mimetype} not supported`)
         p.media = true;
-        await AppDataSource.manager.save(p).catch(err => log.handle(err, 'insert', 'couldnt insert post'))
+        await AppDataSource.manager.save(p).catch(e => log.handle(e, 'insert', 'couldnt insert post'))
         const m = new Media()
-        m.data = file.buffer
+        m.data = f.buffer
         m.post_id = p.id
-        await AppDataSource.manager.save(m).catch(err => log.handle(err, 'inset', 'couldnt insert media'))
-    } else await AppDataSource.manager.save(p).catch(err => log.handle(err, 'insert', 'couldnt insert post'))
+        await AppDataSource.manager.save(m).catch(e => log.handle(e, 'insert', 'insert media error'))
+    } else await AppDataSource.manager.save(p).catch(e => log.handle(e, 'insert', 'insert post error'))
 }
 
-
-function sizeSupported(buff: Buffer): boolean {
-    if (buff.length > 5242880) 
-        throw new Error(`file size exceeds 5MB limit`)
-    console.log(buff.length)
-    return true
-}
