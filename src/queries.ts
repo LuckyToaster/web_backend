4598e7acac8c7e2b3177a9be7d9fea8ae8bbf073
LuckyToast <LuckyToast@github.com>
2023-11-23 16:17:55 +0100

    made some small changes

--- src/queries.ts
+++ src/queries.ts
@@ -3,7 +3,16 @@ import { Post, Payment} from "./orm";
 import { Logger } from "./logger";
 import fs from 'fs'
 
-export { getThread, insertPayment, insertPost, nextPostId, like, dislike, postExists, paymentExists}
+export { 
+    getThread, 
+    insertPayment, 
+    insertPost, 
+    nextPostId, 
+    like, 
+    dislike, 
+    postExists, 
+    paymentExists
+}
 
 const MIMES = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif', 'audio/mp3', 'video/mp4', 'video/webm']
 const log = new Logger('./util.ts');
@@ -18,30 +27,6 @@ async function nextPostId(): Promise<number> {
         .query("select max(id) from post")
         .catch(e => log.handle(e, 'nextPostId', 'cannot select latest id'))
     return maxId[0].max + 1
-}
-
-async function insertPost(msg: string, f?: Express.Multer.File) {
-    const p = new Post()
-    p.msg = msg
-    if (f) {
-        if (f.size > 8388608) throw new Error(`${f.originalname} exceeds 8MB max size`)
-        if (!MIMES.includes(f.mimetype)) throw new Error(`${f.originalname} of type ${f.mimetype} not supported`)
-        const ext = f.mimetype.split('/')[1]
-        fs.writeFile(`./media/${await nextPostId()}.${ext}`, f.buffer, e => log.handle(e, 'insert', 'error saving media'))
-        p.media = true
-        p.mediaMimeType = ext
-    }
-    await AppDataSource.manager.save(p)
-        .catch(e => log.handle(e, 'insertPost'))
-}
-
-async function insertPayment(date: Date, stripeId: string, amount: string) {
-    const p = new Payment()         
-    p.date = date
-    p.stripeId = stripeId 
-    p.amount = amount
-    await AppDataSource.manager.save(p)
-        .catch(e => log.handle(e, 'insertPayment'))
 }
 
 // test this method
@@ -64,4 +49,28 @@ async function like(id: string) {
 async function dislike(id: string) {
     await AppDataSource.getRepository(Post)
         .query(`update post set dislikes = dislikes + 1 where id = ${parseInt(id, 10)}`)
+}
+
+async function insertPayment(date: Date, stripeId: string, amount: string) {
+    const p = new Payment()         
+    p.date = date
+    p.stripeId = stripeId 
+    p.amount = amount
+    await AppDataSource.manager.save(p)
+        .catch(e => log.handle(e, 'insertPayment'))
+}
+
+async function insertPost(msg: string, f?: Express.Multer.File) {
+    const p = new Post()
+    p.msg = msg
+    if (f) {
+        if (f.size > 8388608) throw new Error(`${f.originalname} exceeds 8MB max size`)
+        if (!MIMES.includes(f.mimetype)) throw new Error(`${f.originalname} of type ${f.mimetype} not supported`)
+        const ext = f.mimetype.split('/')[1]
+        fs.writeFile(`./media/${await nextPostId()}.${ext}`, f.buffer, e => log.handle(e, 'insert', 'error saving media'))
+        p.media = true
+        p.mediaMimeType = ext
+    }
+    await AppDataSource.manager.save(p)
+        .catch(e => log.handle(e, 'insertPost'))
 }
